b223eb918d0d123134c6a36c3047e2d151a26865
William Sampaio <1997wbms@gmail.com>
2022-07-25 23:04:24 -0400

    update

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
+import hashlib
 import random
 import os
 import time
@@ -10,42 +11,50 @@ def append_new_line(file_name, text_to_append):
         if len(data) > 0:
             file_object.write("\n")
         file_object.write(text_to_append)
+        return True
 
 
 def apply(path):
+    count = 0
     obj = os.scandir(path)
     for entry in obj:
         if entry.is_dir():
-            apply(entry.path)
+            count = count + apply(entry.path)
         else:
             if len(entry.name) >= 100:
-                new_name = str(time.time()) + str(random.randint(100, 999))
-                append_new_line(
+                new_name = hashlib.md5((entry.name + str(random.randint(100, 999))).encode("utf-8")).hexdigest() + ".novonome"
+                if(append_new_line(
                     "list-files.txt",
                     os.path.dirname(entry.path).replace("\\", "/")
                     + "/"
                     + new_name
                     + "::"
                     + entry.name,
-                )
+                )):
+                    count = count + 1
                 os.rename(
                     entry.path,
-                    os.path.dirname(entry.path).replace("\\", "/") + "/" + new_name,
+                    os.path.dirname(entry.path).replace(
+                        "\\", "/") + "/" + new_name,
                 )
     obj.close()
-    return
+    return count
 
 
 def revert():
+    count = 0
     lines = open("list-files.txt", "r").readlines()
     for line in lines:
         data = line.split("::")
         temp_name = data[0]
         original_name = os.path.dirname(data[0]) + "/" + data[1]
         os.rename(temp_name, original_name.replace("\n", ""))
-    os.remove("list-files.txt")
-    return
+        count = count + 1
+    os.rename("list-files.txt", "OK_list-files.txt")
+    return count
 
+
+os.system('cls')
 
 root_path = os.getcwd()
 print("Diretório: '% s'\n" % root_path)
@@ -56,10 +65,10 @@ print("2. Reverter\n")
 option = input()
 
 if option == "1":
-    apply(root_path)
+    print("Arquivos encontrados: " + str(apply(root_path)))
 else:
     if option == "2":
-        revert()
+        print("Arquivos encontrados: " + str(revert()))
     else:
         print("Opção inválida!")
 
